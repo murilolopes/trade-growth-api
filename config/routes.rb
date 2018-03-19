eebd7aa7f1cac6093992235dc1946fbe4bebd517
Murilo Ângelo <muriloangelo10@gmail.com>
2018-03-19 17:37:03 -0300

    aninhando recurso de objetivos e trades

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  resources :trades
-  resources :goals
+  #resources :trades
+  #resources :goals
+
+  map.resources :goals do |goal|
+    goal.resources :trades
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
