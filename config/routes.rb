e4ddb1fd7932855c6443986ad40b40f7b755c08c
Murilo Ângelo <muriloangelo10@gmail.com>
2018-04-25 14:07:40 -0300

    ajustes de database

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  # resources :trades
-  # resources :goals
+
   resources :goals do
   	resources :trades
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
